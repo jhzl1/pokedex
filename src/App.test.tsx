c833a51597200175fefc140687836cddeeb473f2
Omar <omarjc-1997@hotmail.com>
2022-12-07 17:48:17 -0500

    first commit

---
+++ src/App.test.tsx
@@ -0,0 +1,9 @@
+import { render } from "@testing-library/react"
+import { describe, test } from "vitest"
+import App from "./App"
+
+describe("test at app", () => {
+  test("should render the app", () => {
+    render(<App />)
+  })
+})
