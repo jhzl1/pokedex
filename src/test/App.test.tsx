d9c6a71f3b614160735e8d89664dc15ff8e7c084
omar campos <omarjc-1997@hotmail.com>
2022-12-09 14:25:45 -0500

    udpate

--- src/test/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from "@testing-library/react"
-import { describe, test } from "vitest"
-import { App } from "../App"
-
-describe("test at app", () => {
-  test("should render the app", () => {
-    render(<App />)
-  })
-})
--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,17 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 import tsconfigPaths from "vite-tsconfig-paths"
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   test: {
     globals: true,
     environment: "jsdom",
     setupFiles: "./src/test/setup.ts",
-    // you might want to disable it, if you don't have tests that rely on CSS
-    // since parsing CSS is slow
     css: true,
+  },
+
+  ssr: {
+    // required while deps.fallbackCJS is required
+    noExternal: ["@chakra-ui/react"],
   },
 })
